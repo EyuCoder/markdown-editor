6d82bf72c409065711f51a5b509a69d2356b7565
elysium09 <eyueldaniel0921@gamil.com>
2022-02-22 17:00:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkDown from "react-markdown";
 
 function App() {
+  const [markdown, setMarkdown] = useState("##  Hello Markdown!");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+
+        <article className="preview">
+          <ReactMarkDown>{markdown}</ReactMarkDown>
+        </article>
+      </section>
+    </main>
   );
 }
 
